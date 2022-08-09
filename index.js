fc048ff1f0819761cd3f83a02b971f9ee3307bce
felipe595 <110128997+felipe595@users.noreply.github.com>
2022-08-09 08:56:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const yesBtn= document.querySelector("#yesBtn");
+
+yesBtn.addEventListener('click', function  (){
+    alert('Era obvio que ibas a decir que si(; TE AMO')
+}) 
+
+const noBtn= document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function(){
+    const randomX = parseInt(Math.random()*100); 
+    const randomY = parseInt(Math.random()*100); 
+    noBtn.style.setProperty('top', randomX+'%'); 
+    noBtn.style.setProperty('left', randomY+'%'); 
+    noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`); 
+})
+
+
